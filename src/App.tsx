4af2fda21208b4d12f02049f592ce0b8c5157ca6
timeoutguy <marcus.dev@protonmail.com>
2020-06-29 09:56:17 -0300

    Style global, fonte, readme,  dependencias

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 
+import Layout from './components/Layout';
+import GlobalStyles from './styles/GlobalStyles';
+
 function App() {
   return (
-    <h1>Marcuss</h1>
+    <>
+      <Layout />
+
+      <GlobalStyles />
+    </>
   );
 }
 
